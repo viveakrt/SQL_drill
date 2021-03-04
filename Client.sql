1e6d45b1fcfaa26a7e3a9f42e71a95c6ee2764b3
Vivek <vivek.15@mountblue.tech>
2021-03-04 17:16:11 +0530

    Create schema client if not exists and then create table client with column idClient (Primary key), Name, Location, idManager (Foreign Key)

---
+++ Client.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS `client`;
+
+CREATE TABLE IF NOT EXISTS `client`.`Client` (
+    `idClient` INT NOT NULL AUTO_INCREMENT,
+    `Name` VARCHAR(100) NULL DEFAULT NULL,
+    `Location` VARCHAR(100) NULL DEFAULT NULL,
+    `idManager` INT NULL DEFAULT NULL,
+    PRIMARY KEY (`idClient`),
+    FOREIGN KEY (`idManager`) REFERENCES `client`.`Manager` (`idManager`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);

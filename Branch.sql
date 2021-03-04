af87ab4f3cf03f12d53517eb96f7fa96aa1a41cb
Vivek <vivek.15@mountblue.tech>
2021-03-04 17:00:26 +0530

    Created table Supply with column branch, ISBN, Num_Copies  and table Book with column ISBN (Primary Key), Title, Author, Publisher

--- Branch.sql
+++ Branch.sql
@@ -5,3 +5,22 @@ CREATE TABLE IF NOT EXISTS `branch`.`Branch` (
     `Branch_Addr` VARCHAR(100) NULL DEFAULT NULL,
     PRIMARY KEY (`Branch`)
 );
+
+CREATE TABLE IF NOT EXISTS `branch`.`Book` (
+    `ISBN` INT(20) NOT NULL,
+    `Tittle` VARCHAR(100) NULL DEFAULT NULL,
+    `Author` VARCHAR(100) NULL DEFAULT NULL,
+    `Publisher` VARCHAR(100) NULL DEFAULT NULL,
+    PRIMARY KEY (`ISBN`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `branch`.`Supply` (
+    `Branch` INT(11) NOT NULL,
+    `ISBN` INT(20) NOT NULL,
+    `Num_copies` INT(11) NULL DEFAULT NULL,
+    PRIMARY KEY (`Branch`, `ISBN`),
+    FOREIGN KEY (`Branch`) REFERENCES `mydb`.`Branch` (`Branch`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY (`ISBN`) REFERENCES `mydb`.`Book` (`ISBN`) ON DELETE NO ACTION ON UPDATE NO ACTION
+
+);

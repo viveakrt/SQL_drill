4ebf665f878ab17aa984643255692a4e5302826a
Vivek <vivek.15@mountblue.tech>
2021-03-04 18:25:07 +0530

    Created Patient table with column idPatient (Primary Key), PatientName, PatientDOB, PatientAddess, idDoctor(Foreign key)

--- Doctor.sql
+++ Doctor.sql
@@ -3,7 +3,17 @@ CREATE SCHEMA IF NOT EXISTS `doctor`;
 CREATE TABLE IF NOT EXISTS `doctor`.`Doctor` (
     `idDoctor` INT NOT NULL AUTO_INCREMENT,
     `DoctorName` VARCHAR(100),
-    `Secretory` VARCHAR(100)
+    `Secretory` VARCHAR(100),
     PRIMARY KEY (`idDoctor`)
+);
+
+CREATE TABLE IF NOT EXISTS `doctor`.`Patient` (
+    `idPatient` INT NOT NULL AUTO_INCREMENT,
+    `PatientName` VARCHAR(100),
+    `PatientDOB` DATE,
+    `PatentAddress` VARCHAR(100),
+    `idDoctor` INT NOT NULL,
+    PRIMARY KEY (`idPatient`),
+    FOREIGN KEY (`idDoctor`) REFERENCES `doctor`.`Doctor` (`idDoctor`)
 );
 

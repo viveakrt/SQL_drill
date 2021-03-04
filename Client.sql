b833294c5cb0ba7deccdc097d3a6e1bd6232f5c6
Vivek <vivek.15@mountblue.tech>
2021-03-04 17:26:51 +0530

    Created table Manage with column idManager (Primary Key), Manager_Name, Manager_Location

--- Client.sql
+++ Client.sql
@@ -8,3 +8,10 @@ CREATE TABLE IF NOT EXISTS `client`.`Client` (
     PRIMARY KEY (`idClient`),
     FOREIGN KEY (`idManager`) REFERENCES `client`.`Manager` (`idManager`) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
+
+CREATE TABLE IF NOT EXISTS `client`.`Manager` (
+    `idManager` INT NOT NULL AUTO_INCREMENT,
+    `Manager_Name` VARCHAR(100) NULL DEFAULT NULL,
+    `Manager_Location` VARCHAR(100) NULL DEFAULT NULL,
+    PRIMARY KEY (`idManager`)
+);
